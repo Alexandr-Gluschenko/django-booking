b664ddc023e7f503b9d2f4296df9c7e806aea719
Alexandr <oleksandrglushen2005@gmail.com>
2025-11-25 02:46:34 +0200

    fix: update .gitignore and static settings configuration

--- core/settings/base.py
+++ core/settings/base.py
@@ -152,4 +152,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'apps/static/media')
